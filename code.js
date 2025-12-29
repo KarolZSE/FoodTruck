5aa654e57955eb09afbe6a3519094b98c835f4f2
KarolZSE <karol.sulek@zse.krakow.pl>
2025-12-30 00:17:33 +0100

    Added a counter for your restaurant, added a client, added animations for the client

---
+++ code.js
@@ -0,0 +1,14 @@
+const NPC = document.getElementById('NPC');
+
+NPC.style.backgroundImage = `url('Textures/NPC${Math.floor(Math.random() * 4) + 1}.png')`;
+
+let i = 0;
+function AnimateNPC() {
+    NPC.style.backgroundPosition = `-${(i % 3) * 480}px 0px`;
+    i++;
+    setTimeout(() => {
+        requestAnimationFrame(AnimateNPC);   
+    }, 600);
+}
+
+AnimateNPC();
